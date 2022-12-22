ca7191ee7a03cf912cb8be8439c8f9e785fcb3fb
Kalvis <kalvis@kalmars.lv>
2022-12-22 16:10:26 +0200

    LazyLoad now has loading element before full load

--- resources/js/src/app/components/common/LazyLoad.vue
+++ resources/js/src/app/components/common/LazyLoad.vue
@@ -1,31 +1,32 @@
 <template>
-    <div v-if="enabled && isLoaded">
-        <slot></slot>
-    </div>
+  <div v-if="enabled && isLoaded">
+    <div v-if="isLoaded"><slot></slot></div>
+    <div v-else class="component-loading"></div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "lazy-load",
+  name: "lazy-load",
 
-    props: {
-        component: String
-    },
+  props: {
+    component: String
+  },
 
-    computed: {
-        isLoaded() {
-            return !!this.$store.state.lazyComponent.components[this.component];
-        }
-    },
-
-    data() {
-        return {
-            enabled: false
-        }
-    },
-
-    mounted() {
-        this.enabled = true;
+  computed: {
+    isLoaded() {
+      return !!this.$store.state.lazyComponent.components[this.component];
     }
+  },
+
+  data() {
+    return {
+      enabled: false
+    }
+  },
+
+  mounted() {
+    this.enabled = true;
+  }
 }
 </script>
